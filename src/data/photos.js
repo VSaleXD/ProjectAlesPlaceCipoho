f75308a159c4e7e56eec2624c80fb9e2a792171e
VSaleXD <aleandremoulidouane@apps.ipb.ac.id>
2026-05-28 15:23:11 +0700

    refactor photos.js

--- src/data/photos.js
+++ src/data/photos.js
@@ -1,31 +1,39 @@
-const publicImage = (fileName) => `/gambarHomepage/${fileName}`;
-const publicImageGallery = (fileName) => `/gambarHomepage/Gallery/${fileName}`;
-const withParams = (url) => `${url}?auto=format&fit=crop&w=800&q=80`;
+import HeroBanner from '../../public/gambarHomepage/Hero-Banner.jpg';
+import Logo from '../../public/gambarHomepage/Logo.jpeg';
+import Foto1 from '../../public/gambarHomepage/Gallery/Foto1.jpeg';
+import Foto2 from '../../public/gambarHomepage/Gallery/Foto2.jpeg';
+import Foto3 from '../../public/gambarHomepage/Gallery/Foto3.jpeg';
+import Foto4 from '../../public/gambarHomepage/Gallery/Foto4.jpeg';
+import Foto5 from '../../public/gambarHomepage/Gallery/Foto5.jpeg';
+import Foto6 from '../../public/gambarHomepage/Gallery/foto6.jpg';
+import SuasanaHangat from '../../public/gambarHomepage/Gallery/suasanaHangat.jpeg';
+import AnakSekolah from '../../public/gambarHomepage/Gallery/anakSekolah.png';
+import WifiGratis from '../../public/gambarHomepage/Gallery/wifiGratis.jpeg';
 
 export const SITE_PHOTOS = {
-  hero: publicImage('Hero-Banner.jpg'),
+  hero: HeroBanner,
   atmosphere: [
-    publicImageGallery('Foto2.jpeg'),
-    publicImageGallery('Foto3.jpeg'),
-    publicImageGallery('Foto4.jpeg'),
-    publicImageGallery('Foto5.jpeg'),
+    Foto2,
+    Foto3,
+    Foto4,
+    Foto5,
   ],
   about: [
-    publicImageGallery('suasanaHangat.jpeg'),
-    publicImageGallery('anakSekolah.png'),
-    publicImageGallery('wifiGratis.jpeg'),
+    SuasanaHangat,
+    AnakSekolah,
+    WifiGratis,
   ],
   why: [
-    publicImageGallery('Foto5.jpeg'),
-    publicImageGallery('Foto6.jpeg'),
-    publicImageGallery('Foto1.jpeg'),
+    Foto5,
+    Foto6,
+    Foto1,
   ],
 };
 
 export const ICON_PHOTOS = {
-  instagram: publicImage('Logo.jpeg'),
-  tiktok: publicImage('Logo.jpeg'),
-  facebook: publicImage('Logo.jpeg'),
-  whatsapp: publicImage('Logo.jpeg'),
+  instagram: Logo,
+  tiktok: Logo,
+  facebook: Logo,
+  whatsapp: Logo,
 };
 
